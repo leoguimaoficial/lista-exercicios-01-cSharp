97a9a2e69f67f3fc323cd170bc7720dad4f6d3a0
leoguimaoficial <leonardo.guimaraes13@gmail.com>
2021-10-19 11:54:22 -0300

    ex1

---
+++ exercícios 1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace exercícios_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double nota1, nota2;
+            nota1 = Convert.ToDouble(Console.ReadLine());
+            nota2 = Convert.ToDouble(Console.ReadLine());
+            double resultado = (nota1 + nota2) / 2;
+            if (resultado == 10)
+            {
+                Console.WriteLine("aprovado com dinstinção");
+            }
+            else if (resultado > 7)
+            {
+
+
+
+
+                Console.WriteLine("aprovado");
+            }
+
+
+
+            else
+            {
+                Console.WriteLine("reprovado");
+            }
+
+
+
+
+        }
+
+    }
+}
